975addc3ca91587f9ca177d5e4fb4f6a59136df6
Jamie Kosoy <j.kosoy@bigspaceship.com>
2010-05-28 10:43:36 -0400

    Modified OAuthBridge so initialize will either send back to Flash that you're logged in or not. Before it wouldn't report anything if you weren't logged in.

--- bigspaceship/oauthbridge.js
+++ bigspaceship/oauthbridge.js
@@ -6,29 +6,40 @@ if(!com.bigspaceship.api.facebook) com.bigspaceship.api.facebook = {};
 
 if(!com.bigspaceship.api.facebook.OAuthBridge) {
 	com.bigspaceship.api.facebook.OAuthBridge = {};
-	com.bigspaceship.api.facebook.OAuthBridge.swfId = 'site';	// jk: default swfId is site.
+	com.bigspaceship.api.facebook.OAuthBridge._swfId = 'site';
 
 	com.bigspaceship.api.facebook.OAuthBridge.setSwfId = function($id) {
-		com.bigspaceship.api.facebook.OAuthBridge.swfId = $id;
+		com.bigspaceship.api.facebook.OAuthBridge._swfId = $id;
 	}
 
 	com.bigspaceship.api.facebook.OAuthBridge.initialize = function() {
-		FB.getLoginStatus(function(response) {
-			  if (response.session) {
-					// jk: there is a bug in the current getLoginStatus method Facebook JavaScript SDK -- it doesn't return permissions as expected. the best we can do is manually ask.
-					FB.api({
-				            method : 'fql.query',
-				            query : 'SELECT status_update,photo_upload,sms,offline_access,email,create_event,rsvp_event,publish_stream,read_stream,share_item,create_note,bookmarked,tab_added FROM permissions WHERE uid=' + FB.getSession().uid
-			        },
-			        function(response) {
-						var perms = [];
-						for(perm in response[0]) {
-							if(response[0][perm] == '1') perms.push(perm);
-						}
+		// jk: set a timeout of a second in case getLoginStatus fails.
+		com.bigspaceship.api.facebook.OAuthBridge._timer = setTimeout(function() {
+			$("#" + com.bigspaceship.api.facebook.OAuthBridge._swfId)[0].handleFacebookLogout();
+		},1000);
 
-						$("#" + com.bigspaceship.api.facebook.OAuthBridge.swfId)[0].handleFacebookLogin(FB._apiKey,FB.getSession(),perms.join(','));
-			        });
-				}
+		FB.getLoginStatus(function(response) {
+			clearTimeout(com.bigspaceship.api.facebook.OAuthBridge._timer);
+			com.bigspaceship.api.facebook.OAuthBridge._timer = null;
+			
+			if (response.session) {
+				// jk: there is a bug in the current getLoginStatus method Facebook JavaScript SDK -- it doesn't return permissions as expected. the best we can do is manually ask.
+				FB.api({
+			            method : 'fql.query',
+			            query : 'SELECT status_update,photo_upload,sms,offline_access,email,create_event,rsvp_event,publish_stream,read_stream,share_item,create_note,bookmarked,tab_added FROM permissions WHERE uid=' + FB.getSession().uid
+			       },
+			       function(response) {
+					var perms = [];
+					for(perm in response[0]) {
+						if(response[0][perm] == '1') perms.push(perm);
+					}
+
+					$("#" + com.bigspaceship.api.facebook.OAuthBridge._swfId)[0].handleFacebookLogin(FB._apiKey,FB.getSession(),perms.join(','));
+			       });
+			}
+			else {
+				$("#" + com.bigspaceship.api.facebook.OAuthBridge._swfId)[0].handleFacebookLogout();
+			}
 		});
 	}
 
@@ -37,15 +48,15 @@ if(!com.bigspaceship.api.facebook.OAuthBridge) {
 
 		FB.login(function(response) {
 			if(response.session) {
-				$("#" + com.bigspaceship.api.facebook.OAuthBridge.swfId)[0].handleFacebookLogin(FB._apiKey,FB.getSession(),response.perms);
+				$("#" + com.bigspaceship.api.facebook.OAuthBridge._swfId)[0].handleFacebookLogin(FB._apiKey,FB.getSession(),response.perms);
 			} else {
-				$("#" + com.bigspaceship.api.facebook.OAuthBridge.swfId)[0].handleFacebookLoginCancel();
+				$("#" + com.bigspaceship.api.facebook.OAuthBridge._swfId)[0].handleFacebookLoginCancel();
 			}
 		},$opts);
 	}
 
 	com.bigspaceship.api.facebook.OAuthBridge.logout = function() {
 		FB.logout(function(response) {});
-		$("#" + com.bigspaceship.api.facebook.OAuthBridge.swfId)[0].handleFacebookLogout();
+		$("#" + com.bigspaceship.api.facebook.OAuthBridge._swfId)[0].handleFacebookLogout();
 	}
 }
