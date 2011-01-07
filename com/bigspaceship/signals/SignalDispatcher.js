38591ece987b4144927dba89f4a6c0f61a781dc1
Matt Kenefick <keneficksays@gmail.com>
2011-01-07 16:35:03 -0500

    Cleaned class. Optional Out debug. Added usage. Added comments.

--- com/bigspaceship/signals/SignalDispatcher.js
+++ com/bigspaceship/signals/SignalDispatcher.js
@@ -1,3 +1,84 @@
+/**
+ * DelayBinding by Big Spaceship. 2008-2010
+ *
+ * To contact Big Spaceship, email info@bigspaceship.com or write to us at 45 Main Street #716, Brooklyn, NY, 11201.
+ * Visit http://labs.bigspaceship.com for documentation, updates and more free code.
+ *
+ *
+ * Copyright (c) 2008-2010 Big Spaceship, LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ **/
+
+
+/**
+ * SignalDispatcher
+ *
+ * @copyright       2010 Big Spaceship, LLC
+ * @author          Matt Kenefick
+ * @version         1.0
+ * @langversion     Javascript
+ *
+ *
+ *  Usage:
+ *
+ *  SignalDispatcher allows you to communicate indirectly with other parts
+ *  of your code and attach multiple callbacks to an event.
+
+
+    SignalDispatcher.addSignal('mouse', 'CLICK', _mouse_CLICK_handler,  { time: Date.now() });
+    SignalDispatcher.addSignal('mouse', 'CLICK', _mouse_CLICK2_handler, { time: Date.now() });
+    SignalDispatcher.addSignal('mouse', 'CLICK', _mouse_CLICK3_handler, { time: Date.now() });
+
+    document.onclick    =   function onclick($e){
+
+        // send to all 3 functions. They all fire with the $e attached.
+        SignalDispatcher.sendSignal('mouse', 'CLICK', {event: $e});
+
+    };
+
+    function _mouse_CLICK_handler($e, $data, $args){
+        var setByListener   =   $data;
+        var setByEvent      =   $args;
+
+        alert( "Fire 1 : " + setByListener.time + " : " + setByEvent.event.pageX );
+    };
+
+    function _mouse_CLICK2_handler($e, $data, $args){
+        var setByListener   =   $data;
+        var setByEvent      =   $args;
+
+        alert( "Fire 2 : " + setByListener.time + " : " + setByEvent.event.pageX );
+    };
+
+    function _mouse_CLICK3_handler($e, $data, $args){
+        var setByListener   =   $data;
+        var setByEvent      =   $args;
+
+        alert( "Fire 3 : " + setByListener.time + " : " + setByEvent.event.pageX );
+    };
+ *
+ *
+ */
+
+
 var Signal          =   {
     COMPLETE:           "signalComplete",
     READY:              "signalReady",
@@ -14,10 +95,36 @@ var MouseSignal     =   {
 };
 
 
- var SignalDispatcher   = new (function(){
+var SignalDispatcher    =   new (function(){
+
+    // private vars
+    var _self           =   this;
+
+    // public vars
+    this.name           =   "SignalDispatcher";
     this.listenerChain  =   {};
 
-    this.addSignal = function($object, $type, $listener, $data){
+
+// ===========================================
+// ===== CALLABLE
+// ===========================================
+
+    /**
+     * The <code>addSignal</code> method
+     *
+     * Creates a signal that we should listen for. You can assign it to
+     * anything and provide additional arguments at set time that can be
+     * retrieved by the callback functions.
+     *
+     * @access public
+     * @param  $object   Mixed      Any type of reference/namespace.
+     * @param  $type     String     Event name
+     * @param  $listener Function   Callback function to fire.
+     * @param  $data     Object     Optional object of data to pass to the callback
+     * @return _self
+     */
+
+    this.addSignal      =   function addSignal($object /*Mixed*/, $type /*String*/, $listener /*Function*/, $data /*Object*/){
         var listenerObj =   {};
 
         // save data
@@ -40,9 +147,23 @@ var MouseSignal     =   {
             this.listenerChain[$type] = [listenerObj];
         else
             this.listenerChain[$type].push(listenerObj);
+
+        return _self;
     };
 
-    this.hasSignal = function($object, $type){
+
+    /**
+     * The <code>hasSignal</code> method
+     *
+     * Check if a listener exists for a type of namespace.
+     *
+     * @access  public
+     * @param   $object    Mixed    Namespace of listener
+     * @param   $type      String   Event name
+     * @return  boolean
+     */
+
+    this.hasSignal      =   function hasSignal($object, $type){
         var i;
 
         for( i in this.listenerChain[$type] ){
@@ -52,6 +173,19 @@ var MouseSignal     =   {
 
         return false;
     };
+
+
+    /**
+     * The <code>removeSignal</code> method
+     *
+     * Remove a listener from a namespace
+     *
+     * @access  public
+     * @param   $object    Mixed    Namespace of listener
+     * @param   $type      String   Event name
+     * @param   $listener  Function Function to remove
+     * @return  _self
+     */
 
     this.removeSignal = function($object, $type, $listener){
         var i;
@@ -65,7 +199,23 @@ var MouseSignal     =   {
                     this.listenerChain[$type].splice(i, 1);
              }
 
+        return _self;
     };
+
+
+    /**
+     * The <code>sendSignal</code> method
+     *
+     * Sends a signal to all listeners attached to that
+     * namespace / event combination. Allows for arguments
+     * at send time.
+     *
+     * @access  public
+     * @param   $object    Mixed    Namespace of listener
+     * @param   $type      String   Event name
+     * @param   $args      Object   Additional args to send to callback
+     * @return  boolean
+     */
 
     this.sendSignal = function($object, $type, $args){
         var i;
@@ -83,17 +233,27 @@ var MouseSignal     =   {
                     this.listenerChain[$type][i].type
                 );
             }else{
-                Out.warning("No listener on: " + $object + "/" + $type + "/" + this.listenerChain[$type].name);
+                if(Out)
+                    Out.warning(_self, "No listener on: " + $object + "/" + $type + "/" + this.listenerChain[$type].name);
             }
         };
 
+        return _self;
     };
 
-    this.test       =   function(){
-        alert('testing');
-    };
 
-    this.attach     =   function($object){
+    /**
+     * The <code>attach</code> method
+     *
+     * You can add this class to the __proto__ of objects if you wish. Be careful
+     * when iterating and for Browser compaitibilities
+     *
+     * @access  public
+     * @param   $object    Object
+     * @return  void
+     */
+
+    this.attach         =   function attach($object){
         $object.__proto__.addSignal      =   function addSignal($type, $listener, $data){
             return SignalDispatcher.addSignal(this, $type, $listener, $data);
         };
@@ -111,21 +271,3 @@ var MouseSignal     =   {
         };
     };
 })();
-
-
-
-
-// Testing...
-/*
-var bob = new (function(){this.name='bob!';return this;})();
-
-bob.addSignal('Mouse', function($e, $data, $args){
-
-    alert($data.myData);
-    alert( $args.ok );
-
-}, {myData: 'whats up'} );
-
-
-bob.sendSignal( 'Mouse' );
-*/
